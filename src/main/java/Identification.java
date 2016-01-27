6552b9642ba2273557f8b73540df0169ed9c4d07
many31 <many31@130.120.214.240>
2016-01-27 16:52:45 +0100

    Identification : CONNEXION

--- src/main/java/Identification.java
+++ src/main/java/Identification.java
@@ -12,31 +12,50 @@ import java.util.*;
  *
  * @author Emmanuel Ménat
  */
-public class Identification {
+public class Identification 
+{
     Connection conn;
 	private String identifiant;
         private String mdp;
-	public Identification(String identifiant,String mdp) {
+	
+       public Identification(String identifiant,String mdp) 
+        {
             this.identifiant=identifiant;
             this.mdp=mdp;
-		}
+        }
         
-        public void connexion(){  
-                 try {
-		    Class.forName("com.mysql.jdbc.Driver");
-		    Connection con = DriverManager.getConnection("jdbc:mysql://binary-digit.net:3305/ServeurIdentification", "yahimenat", "odaime");
-		    // on cree un objet Statement qui va permettre l'execution des requetes
-	        Statement s = con.createStatement();
-	        try {
-	        	ResultSet compteID;
-                        compteID = s.executeQuery("SELECT id_compte FROM compte WHERE login = "+identifiant+" AND  password="+mdp+";)");
-	        } catch(Exception e) {
-	        	// sinon on l'a cree
+        public int connexion()
+        {  
+            int resultat = 0;
+                 try 
+                 {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection con = DriverManager.getConnection("jdbc:mysql://binary-digit.net:3305/ServeurIdentification", "yahimenat", "odaime");
+                    // on cree un objet Statement qui va permettre l'execution des requetes
+                     Statement s = con.createStatement();
+                    try {
+                            ResultSet compteID;
+                            compteID = s.executeQuery("SELECT id_compte FROM compte WHERE login = "+identifiant+" AND  password="+mdp+";)");
+                            if (!compteID.next())
+                            {
+                                resultat=-1;
+                            } 
+                            else 
+                            {
+                                resultat=compteID.getInt("id_compte");
+                            }
+                        } 
+                    catch(Exception e) 
+                    {
+                    // sinon on l'a cree
 	               
-	        }
-		} catch(ClassNotFoundException | SQLException e) {
+                    }
+		} 
+                catch(ClassNotFoundException | SQLException e) 
+                {
 		}
-}
+            return resultat;
+        }
         //Fermer connection
 	public void fermer() throws Exception {		
 		try {
