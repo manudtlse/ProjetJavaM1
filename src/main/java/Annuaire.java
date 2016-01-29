8a3525fe6f9d84f0c5b258a439bb162a08382d5e
many31 <many31@130.120.214.240>
2016-01-29 13:47:22 +0100

    Lireinfos(),InsererInfos(),MajInfos() --> OK Annuaire.java, tout fonctionne

--- src/main/java/Annuaire.java
+++ src/main/java/Annuaire.java
@@ -13,6 +13,7 @@
 //http://blog.paumard.org/cours/jdbc/chap02-apercu-exemple.html
 
 
+
 import java.sql.*;
 import java.util.*;
 
@@ -22,42 +23,63 @@ public class Annuaire {
         private String url2="jdbc:mysql://binary-digit.net:3306/yahimenat";//ServeurAnnuaire
         private String bdlogin="yahimenat";
         private String bdmdp="odaime";
+        Connection con;
+                      
 	
-	
-	Connection conn;
-	
-	public Annuaire() 
-        {
-        }
-
-	public String lireInfos(String nom) {
+	public Annuaire() {
 		try {
-			Statement s = conn.createStatement();
-			ResultSet rs = s.executeQuery("select * from ANNUAIRE where nom = '"+nom+"'");
+		     Class.forName("com.mysql.jdbc.Driver");
+                    Connection con = DriverManager.getConnection(url2,bdlogin,bdmdp);
+                    // on cree un objet Statement qui va permettre l'execution des requetes
+                     Statement s = con.createStatement();
+		} catch(Exception e) {
+			// il y a eu une erreur
+			e.printStackTrace();
+		}
+	}
+ 
+                    
+        
+
+	public String lireInfos(int numetudiant) {
+		try {	
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection con = DriverManager.getConnection(url2,bdlogin,bdmdp);
+                    Statement s = con.createStatement();
+			ResultSet rs = s.executeQuery("select * from profil_etudiant where num_etudiant = '"+numetudiant+"';");
 	        if (rs.next()) {
-	        	return rs.getString("nom")+":"+rs.getString("telephone")+":"+rs.getString("eMail");
+	        	return rs.getInt("num_etudiant")+":"+rs.getString("nom_etudiant")+":"+rs.getString("prenom_etudiant")+":"+rs.getString("date_naissance")+":"+rs.getString("mail")+":"+rs.getString("telephone")+":"+rs.getInt("id_competence");
 	        } else {
-	        	return null;
+                    return null;
+
 	        }
 		} catch(Exception ex) {
 			// il y a eu une erreur
 			ex.printStackTrace();
-			return null;
+                        return null;
+         
+
 		}
 	}
 
 	public boolean ajoutInfos(String infos) {
 		try {
 			String [] tab = infos.split(":");
-			if(tab.length != 3) 
+			if(tab.length != 6) 
 				return false;
-			String nom = tab[0];
-			String telephone = tab[1];
-			String eMail = tab[2];
+                        String nom_etudiant = tab[0];
+			String prenom_etudiant = tab[1];
+			String date_naissance = tab[2];
+                        String mail = tab[3];
+                        String telephone = tab[4];
+                        String id_comp = tab[5]; //a convertir en int pour la BD
+                        Integer id_competence=Integer.parseInt(tab[5]);
 			
-			Statement s = conn.createStatement();
-			if (s.executeUpdate("insert into ANNUAIRE values ('"+nom+"', '"+telephone+"', '"+eMail+"')")==1)
-				return true;
+                        Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection(url2,bdlogin,bdmdp);
+			Statement s = con.createStatement();
+			if (s.executeUpdate("INSERT INTO profil_etudiant(nom_etudiant,prenom_etudiant,date_naissance,mail,telephone,id_competence) VALUES ('"+tab[0]+"','"+tab[1]+"','"+tab[2]+"','"+tab[3]+"','"+tab[4]+"','"+id_competence+"');")==1)
+                            return true;
 			else
 				return false;
 		} catch(Exception ex) {
@@ -70,14 +92,21 @@ public class Annuaire {
 	public boolean majInfos(String infos) {
 		try {
 			String [] tab = infos.split(":");
-			if(tab.length != 3) 
+			if(tab.length != 7) 
 				return false;
-			String nom = tab[0];
-			String telephone = tab[1];
-			String eMail = tab[2];
+                        String nom_etudiant = tab[1];
+			String prenom_etudiant = tab[2];
+			String date_naissance = tab[3];
+                        String mail = tab[4];
+                        String telephone = tab[5];
+                        Integer id_competence=Integer.parseInt(tab[6]);               //converti en int pour la BD
+
+                        Integer num_etudiant=Integer.parseInt(tab[0]);
 			
-			Statement s = conn.createStatement();
-			if (s.executeUpdate("update ANNUAIRE set telephone='"+telephone+"', eMail='"+eMail+"' where nom='"+nom+"'")==1)
+			Class.forName("com.mysql.jdbc.Driver");
+                        Connection con = DriverManager.getConnection(url2,bdlogin,bdmdp);
+			Statement s = con.createStatement();
+			if (s.executeUpdate("update profil_etudiant set  nom_etudiant='"+nom_etudiant+"',prenom_etudiant='"+prenom_etudiant+"',date_naissance='"+date_naissance+"',mail='"+mail+"',telephone='"+telephone+"',id_competence='"+id_competence+"' where num_etudiant="+num_etudiant+";")==1)
 				return true;
 			else
 				return false;	
@@ -90,12 +119,25 @@ public class Annuaire {
 	
 	public void fermer() throws Exception {		
 		try {
-			conn.close();
+			con.close();
 		} catch(Exception ex) {
 			// il y a eu une erreur
 			ex.printStackTrace();
 		}
 	}
-	
+ public static void main(String[] args) throws Exception 
+        {
+	    String resultat;
+            Boolean result;
+            Annuaire an1;
+            an1 = new Annuaire();
+            resultat=an1.lireInfos(1);
+            System.out.println("Résultat : "+resultat);
+            result=an1.ajoutInfos("nom2:prenom2:18/12/1993:nom2.prenom2@gmail.com:0665758387:1");
+            an1.majInfos("5:nom4:prenom4:18/12/1993:nom4.prenom4@gmail.com:0665758387:1");
+            System.out.println(result);
+	}	
+
+    
 	
 }
